be2b0adaf9ef27ef96e9648cbf595f7c1e3cfc9c
atsond12 <atsond12@student.uia.no>
2025-01-24 12:07:19 +0100

    Laget nedtelling

--- module_5/FlappyBird/menu.mjs
+++ module_5/FlappyBird/menu.mjs
@@ -1,5 +1,6 @@
 "use strict";
 import lib2d from "../../common/libs/lib2d.mjs";
+import libSound from "../../common/libs/libSound.mjs";
 import libSprite from "../../common/libs/libSprite.mjs";
 import { SpriteInfoList, GameProps, EGameStatus } from "./FlappyBird.mjs";
 
@@ -11,12 +12,14 @@ Lage en play knapp som kan starte spillet.
 export class TMenu {
   #spFlappyBird;
   #spButtonPlay;
+  #spNumber;
   #spInfoText;
   #spcvs;
   #activeSprite;
   constructor(aSpriteCanvas) {
     this.#spcvs = aSpriteCanvas;
     const pos = new lib2d.TPosition(200, 100);
+    //GameProps.status = EGameStatus.getReady;
 
     this.#spFlappyBird = new libSprite.TSprite(aSpriteCanvas, SpriteInfoList.flappyBird, pos);
     pos.y = 200;
@@ -28,6 +31,10 @@ export class TMenu {
     pos.y = 100;
     this.#spInfoText = new libSprite.TSprite(aSpriteCanvas, SpriteInfoList.infoText, pos);
 
+    pos.x = 285;
+    pos.y = 180;
+    this.#spNumber = new libSprite.TSprite(aSpriteCanvas, SpriteInfoList.numberBig, pos);
+    this.#spNumber.index = 3; //Nedtelling starter på 3
     this.#spcvs.addEventListener("mousemove", this.#onMouseMove);
     this.#spcvs.addEventListener("click", this.#onClick);
     this.#activeSprite = null; //Vi har ingen aktive sprite enda, når musen er over en sprite setter vi denne til den aktive sprite
@@ -41,6 +48,7 @@ export class TMenu {
         break;
       case EGameStatus.getReady:
         this.#spInfoText.draw();
+        this.#spNumber.draw();
         break;
     }
   } // end of draw
@@ -66,6 +74,17 @@ export class TMenu {
     if (this.#activeSprite === this.#spButtonPlay) {
       GameProps.status = EGameStatus.getReady;
       this.#spcvs.style.cursor = "default";
+      setTimeout(this.#onCountDown, 1000);
     }
   };
-}
+
+   #onCountDown = () =>{
+    if(this.#spNumber.index > 1){
+      this.#spNumber.index--;      
+      setTimeout(this.#onCountDown, 1000);
+    }else{
+      GameProps.status = EGameStatus.playing;
+    }
+   }
+
+}// End of TMenu class
