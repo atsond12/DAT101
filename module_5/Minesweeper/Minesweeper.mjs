e8a32721d91705e249b089c010cafbd6459366ce
atsond12 <atsond12@student.uia.no>
2025-02-07 12:06:57 +0100

    Oppdatering workshop

--- module_5/Minesweeper/Minesweeper.mjs
+++ module_5/Minesweeper/Minesweeper.mjs
@@ -24,7 +24,7 @@ export const SpriteInfoList = {
 };
 
 const Difficulty = {
-  Level_1: { Tiles: { Row: 10, Col: 10 }, Mines: 4, caption: "Level 1" },
+  Level_1: { Tiles: { Row: 10, Col: 10 }, Mines: 20, caption: "Level 1" },
   Level_2: { Tiles: { Row: 15, Col: 15 }, Mines: 20, caption: "Level 2" },
   Level_3: { Tiles: { Row: 20, Col: 30 }, Mines: 99, caption: "Level 3" },
 };
