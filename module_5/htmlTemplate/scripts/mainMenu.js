ce85c3b50a8a4fcf8d7447c027ffb15dc9af6f56
Arne-Thomas Aas Søndeled <atsond12@uia.no>
2025-03-11 15:31:58 +0100

    Ny workshop HTML, CSS Shadow DOM og JavaScript

--- module_5/htmlTemplate/scripts/mainMenu.js
+++ module_5/htmlTemplate/scripts/mainMenu.js
@@ -1,17 +1,16 @@
 "use strict";
 import TBootstrapComponent from "./bootstrapComponent.js";
 
-class THome extends TBootstrapComponent {
+class TMainMenu extends TBootstrapComponent {
   constructor(){
     super();
     this.attachShadow({mode: "open"});
   }
 
   render(){
-    const template = document.getElementById("home-page-template");
+    const template = document.getElementById("main-menu-template");
     const content = template.content.cloneNode(true);
     this.shadowRoot.appendChild(content);
   }
 }
-
-customElements.define("home-page", THome);
+customElements.define("main-menu", TMainMenu);
