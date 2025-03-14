94191b59845fd9de9a5fbf295c1fd58790623866
Arne-Thomas Aas Søndeled <atsond12@uia.no>
2025-03-14 08:16:49 +0100

    Oppdatering workshop

--- module_5/htmlTemplate/scripts/mainMenu.js
+++ module_5/htmlTemplate/scripts/mainMenu.js
@@ -11,6 +11,19 @@ class TMainMenu extends TBootstrapComponent {
     const template = document.getElementById("main-menu-template");
     const content = template.content.cloneNode(true);
     this.shadowRoot.appendChild(content);
+    this.#createMenuManager();
+  }
+
+  #createMenuManager(){
+    const menuItems = this.shadowRoot.querySelectorAll("a[page-data]");
+    for(let i = 0; i < menuItems.length; i++){
+      const menuItem = menuItems[i];
+        //Vi må lage en ikke navngitt pilfunksjon pga. eventlytteren
+        menuItem.addEventListener("click",
+        () => {
+          console.log("Navigating to " + menuItem.getAttribute("page-data"));
+        });
+    }
   }
 }
 customElements.define("main-menu", TMainMenu);
