cf33ade9192b44b2b47af12dc0a4a93c6320d578
Arne-Thomas Aas Søndeled <atsond12@uia.no>
2025-03-21 10:59:57 +0100

    Ny bildekarusell script fil pg bilder

---
+++ module_5/htmlTemplate/scripts/pictureCollection.js
@@ -0,0 +1,16 @@
+"use strict";
+import TBootstrapComponent from "./bootstrapComponent.js";
+
+const pictureFiles = 
+[
+  { id:  1, fn: "./img/img_01.png", caption: "Colorado mountain  1" },
+  { id:  2, fn: "./img/img_02.png", caption: "Colorado Horseshoe bend" },
+  { id:  3, fn: "./img/img_03.png", caption: "Colorado mountain  2" },
+  { id:  4, fn: "./img/img_04.png", caption: "European mountain  1" },
+  { id:  5, fn: "./img/img_05.png", caption: "European mountain  2" },
+  { id:  6, fn: "./img/img_06.png", caption: "European mountain  3" },
+  { id:  7, fn: "./img/img_07.png", caption: "European mountain  4" },
+  { id:  8, fn: "./img/img_08.png", caption: "European mountain  5" },
+  { id:  9, fn: "./img/img_09.png", caption: "Grand Canyon  1" },
+  { id: 10, fn: "./img/img_10.png", caption: "Grand Canyon  2" },
+];
