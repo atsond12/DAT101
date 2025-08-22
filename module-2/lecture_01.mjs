523c2e24f9cec6a6c40aec985c9f0d5943feb988
Arne-Thomas Aas Søndeled <atsond12@uia.no>
2025-08-22 13:42:58 +0200

    Test for å vise studentene push og commit

--- module-2/lecture_01.mjs
+++ module-2/lecture_01.mjs
@@ -2,6 +2,7 @@
 "use strict"
 import {printOut} from "../../common/script/utils.mjs";
 
-
-
-
+//Create a function that print hello World to console
+function printHelloWorld() {
+    printOut("Hello World");
+}
