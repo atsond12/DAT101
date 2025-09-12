bb6216720c8106fee099c871275f8190f216ad5b
Arne-Thomas Aas Søndeled <atsond12@uia.no>
2025-09-12 16:08:00 +0200

    Hands On Oppgave 2

--- module-2/task_2-2/task_2-2.mjs
+++ module-2/task_2-2/task_2-2.mjs
@@ -4,23 +4,55 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const part1Ans = (2 + 3) * 2 - 4 * 6;
+printOut("(2 + 3) * 2 - 4 * 6 = " + part1Ans);
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const millimeters = 25000 + 340;
+const inchPrMillimeters = 25.4;
+let sumPart2 = millimeters / inchPrMillimeters;
+sumPart2 = sumPart2.toFixed(2);
+printOut("25 metres and 34 centimeters is " + sumPart2 + " inches");
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/*Convert 3 days, 12 hours, 14 minutes, and 45 seconds to minutes*/
+const part3Days = 3, part3Hours = 14, part3Minutes = 14, part3Seconds = 34;
+let part3Answer = 
+  (part3Days * 24 * 60) +
+  (part3Hours * 60) + 
+  part3Minutes +
+  (part3Seconds / 60)
+part3Answer = part3Answer.toFixed(2);
+printOut("3 days, 12 hours, 14 minutes, and 45 seconds is " + part3Answer + "minutes");
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+/* Convert 6,322.52 minutes to days, hours, minutes, and seconds*/
+const part4Minutes = 6322.52;
+let part4Rest = part4Minutes / (24 * 60);
+const part4Days = Math.floor(part4Rest);
+part4Rest = part4Rest - part4Days;
+part4Rest = part4Rest * 24;
+const part4Hours = Math.floor(part4Rest);
+part4Rest = part4Rest - part4Hours;
+part4Rest = part4Rest * 60;
+const part4Minute = Math.floor(part4Rest);
+part4Rest -= part4Minute;
+part4Rest *= 60;
+const part4Seconds = Math.floor(part4Rest);
+
+printOut("6,322.52 minutes is " + 
+  part4Days + " days, " + 
+  part4Hours + " hours, " +
+  part4Minute + " minutes, " +
+  part4Seconds + " seconds"
+
+);
+
+  printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
