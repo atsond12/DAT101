4f95ff139fb17b4607e4f9beaf44a7d0e46cc300
atsond12 <atsond12@uia.no>
2026-03-09 08:17:36 +0100

    Oppdatering workshop

--- module-5/Task_5-4_Minesweeper/tile.js
+++ module-5/Task_5-4_Minesweeper/tile.js
@@ -5,18 +5,85 @@ import { gameLevel } from "./Minesweeper.mjs";
 
 let tiles = [];
 
-export class TTile extends TSpriteButton{
+export class TTile extends TSpriteButton {
+  #mine;
+  constructor(aSpcvs, aSPI, aCol, aRow) {
+    const pos = new TPoint(20, 133);
+    pos.x += aSPI.width * aCol;
+    pos.y += aSPI.height * aRow;
+    super(aSpcvs, aSPI, pos.x, pos.y);
+    this.#mine = false;
+  }
 
-    constructor(aSpcvs, aSPI){
-        super(aSpcvs, aSPI, 20, 133);
+  get isMine() {
+    return this.#mine;
+  }
+
+  set isMine(aValue) {
+    this.#mine = aValue;
+  }
+
+  get open() {
+    return this.index === 2;
+  }
+
+  // Override functions
+  onMouseDown(eEvent) {
+    this.index = 1;
+    super.onMouseDown(eEvent);
+  }
+
+  onMouseUp(aEvent) {
+    this.index = 2;
+    super.onMouseUp(aEvent);
+  }
+
+  onMouseLeave(aEvent) {
+    if (this.open === false) {
+      this.index = 0;
     }
-
+    super.onMouseLeave(aEvent);
+  }
 } // End of TTile
 
-export function createTiles(){
-  console.log(gameLevel);
+export function createMines() {
+  mineCount = 0;
+  colCount = gameLevel.Tiles.Col;
+  rowCount = gameLevel.Tiles.Row;
+  do {
+    const col = Math.floor(Math.random() * colCount);
+    const row = Math.floor(Math.random() * rowCount);
+    const tile = tiles[col][row];
+    if (tile.isMine === false) {
+      tile.isMine = true;
+      mineCount++;
+    }
+  } while (mineCount <= gameLevel.Mines);
 }
 
-export function drawTiles(){
+export function createTiles(aSpcvs, aSPI) {
+  console.log(gameLevel);
+  const glTiles = gameLevel.Tiles;
+  const colCount = glTiles.Col;
+  const rowCount = glTiles.Row;
+  for (let col = 0; col < colCount; col++) {
+    const rows = [];
+    for (let row = 0; row < rowCount; row++) {
+      const newTile = new TTile(aSpcvs, aSPI, col, row);
+      rows.push(newTile);
+    }
+    tiles.push(rows);
+  }
+}
 
+export function drawTiles() {
+  const colCount = gameLevel.Tiles.Col;
+  const rowCount = gameLevel.Tiles.Row;
+  for (let col = 0; col < colCount; col++) {
+    const rows = tiles[col];
+    for (let row = 0; row < rowCount; row++) {
+      const tile = rows[row];
+      tile.draw();
+    }
+  }
 }
