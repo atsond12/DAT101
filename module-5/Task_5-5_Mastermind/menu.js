ad4fd4c603558510ccefa840c44b92b9ec914cc2
atsond12 <atsond12@uia.no>
2026-03-17 14:22:22 +0100

    Oppdatering Workshop

--- module-5/Task_5-5_Mastermind/menu.js
+++ module-5/Task_5-5_Mastermind/menu.js
@@ -12,12 +12,25 @@ import { TSprite, TSpriteButtonHaptic } from "libSprite";
 export class TMenu{
   #background;
   #newGame;
+  #checkAnswer;
+  #cheat;
+  #hideAnswer;
 
   constructor(){
     this.#background = new TSprite(spcvs, SpriteInfoList.Board, 0, 0);
     let x = MastermindBoard.ButtonNewGame.x;
     let y = MastermindBoard.ButtonNewGame.y;
     this.#newGame = new TSpriteButtonHaptic(spcvs, SpriteInfoList.ButtonNewGame, x, y);
+    x = MastermindBoard.ButtonCheckAnswer.x;
+    y = MastermindBoard.ButtonCheckAnswer.y;
+    this.#checkAnswer = new TSpriteButtonHaptic(spcvs, SpriteInfoList.ButtonCheckAnswer, x, y);
+    x = MastermindBoard.ButtonCheat.x;
+    y = MastermindBoard.ButtonCheat.y;
+    this.#cheat = new TSpriteButtonHaptic(spcvs, SpriteInfoList.ButtonCheat, x, y);
+    x = MastermindBoard.PanelHideAnswer.x;
+    y = MastermindBoard.PanelHideAnswer.y;
+    this.#hideAnswer = new TSprite(spcvs, SpriteInfoList.PanelHideAnswer, x, y);
+
   }
 
   drawBackground(){
