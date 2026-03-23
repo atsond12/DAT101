37318c1fd06ba402ddc7bdb2d45c59104b5f3e3f
atsond12 <atsond12@uia.no>
2026-03-23 09:08:12 +0100

    Oppdatering Workshop

--- module-5/Task_5-5_Mastermind/colorPicker.js
+++ module-5/Task_5-5_Mastermind/colorPicker.js
@@ -1,12 +1,42 @@
 "use strict";
-import { TSpriteDraggable } from "libSprite";
+import { TSpriteDraggable, TSnapTo } from "libSprite";
 import { spcvs, SpriteInfoList, colorPickers } from "./Mastermind.mjs";
 import { MastermindBoard } from "./MastermindBoard.mjs";
+import { TPoint } from "lib2d";
+
+let playerAnswerRow = MastermindBoard.PlayerAnswer.Row1;
+
+class TSnapping extends TSnapTo {
+
+    constructor(){
+      super(playerAnswerRow, 30);
+    }
+
+}
 
 export class TColorPicker extends TSpriteDraggable {
   constructor(aPos) {
     super(spcvs, SpriteInfoList.ColorPicker, aPos.x, aPos.y);
-		this.canDrop = false;
+    this.snapTo = new TSnapping();
+    this.initPos = new TPoint(aPos.x, aPos.y);
+  }
+
+  duplicate(){
+    const newColorPicker = new TColorPicker(this.initPos);
+    newColorPicker.index = this.index;
+    return newColorPicker;
+  }
+
+  canDrop(aPos){
+    for(let i = 0; i < playerAnswerRow.length; i++){
+      const pos = playerAnswerRow[i];
+      if((aPos.x === pos.x) && (aPos.y === pos.y)){
+        const dup = this.duplicate();
+        colorPickers.push(dup);
+        return true;
+      }
+    }
+    return false;
   }
 
   onStartDrag() {
